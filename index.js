bf6de5f80bae76e32165191f598f95f4bbd98b5a
mohdnajwan <najwan97@gmail.com>
2020-04-20 00:05:37 +0800

    CORS Fixed

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ function loadWeather(){
                 let apiKey = "d8f895a440cc6ab8db64e12cd3f4adca";
                 let lat = cord.latitude;
                 let lon = cord.longitude;
-                let url = `https://openweathermap.org/api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+                let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
                 fetch(url)
-                .then(res=>res.json)
+                .then(res=>res.json())
                 .then(data=>{
-                    console.log(data.main.temp);
+                    document.getElementById('temp').innerHTML = data.main.temp;
                 })
             } catch (error) {
                 
